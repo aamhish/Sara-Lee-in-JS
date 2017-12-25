2625a12a88956ca64ea966a37f2bb15201df676f
Aamhish Rao <aamhish@users.noreply.github.com>
2017-12-25 16:54:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,22 @@
 
 var Discord = require('discord.io');
-var logger = require('winston');
+//var logger = require('winston');
 var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
-logger.level = 'debug';
+//logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
 bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+  //  logger.info('Connected');
+ //   logger.info('Logged in as: ');
+ //   logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
